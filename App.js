fb8960d8e2ff34d00d6f6eb981656fa586b2b54d
hizhizhiz <hizhizhizhh@gmail.com>
2018-06-12 18:51:22 +1000

    outline

--- App.js
+++ App.js
@@ -5,9 +5,24 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.top}>
+          <Text style={styles.topText}>TripClick</Text>
+        </View>
+
+        <View style={styles.content}>
+          <Text style={styles.contentText}>Маяк(Бесплатно)</Text>
+          <Text style={styles.contentText}>Фуникулер(Бесплатно)</Text>
+          <Text style={styles.contentText}>Маршрут в разработке</Text>
+          <Text style={styles.contentText}>Маршрут в разработке</Text>
+          <Text style={styles.contentText}>Маршрут в разработке</Text>
+        </View>
+
+        <View style={styles.bottom}>
+          <Text style={styles.bottomText}>Маршруты</Text>
+          <Text style={styles.bottomText}>Купленное</Text>
+          <Text style={styles.bottomText}>Избранное</Text>
+          <Text style={styles.bottomText}>Настройки</Text>
+        </View>
       </View>
     );
   }
@@ -17,7 +32,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  top: {
+    backgroundColor: '#3d81ee',
+    height: '12%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topText: {
+    fontSize: 23,
+    color: '#fff',
+    paddingTop: 20,
+    width: 110,
+  },
+  content: {
+    backgroundColor: '#99cdff',
+    height: '78%',
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+  contentText: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: 2,
+    color: '#fff',
+    fontSize: 20,
+    paddingTop: 55,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  bottom: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    height: '10%',
+    width: '100%',
+  },
+  bottomText: {
+    color: '#767676',
+  }
 });
